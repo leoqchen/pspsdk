b3f009585326d9726c188d317186fc8624599dad
sajattack <sajattack@users.noreply.github.com>
2020-10-04 23:30:06 +0000

    deploy: 0d3c974dd17f20f80321496eaf03c832035dae32

---
+++ navtreedata.js
@@ -0,0 +1,82 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PSPSDK", "index.html", [
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"alloc_8c.html",
+"functions_vars_z.html",
+"group__Ctrl.html#ggac080131ea3904c97efb6c31b1c4deb10a17d94c81e78bb8152fcbec71514871c9",
+"group__GU.html#ga01a1668e175f969f9b958545f62b7c20",
+"group__GU.html#gaee7f589ac1b5ee7ea5c19c88ed2c84b6",
+"group__ModuleMgr.html#ga4572fedc91a1809d94cdb5f97bc4928f",
+"group__Syscon.html#ga80700bb63bd56ebabbb4728aa433fd29",
+"group__ThreadMan.html#gabba95470fda6fdb60aa17b58905e9698",
+"group__UMD.html#gga9c5d054a20187635e557b261de714e42a143876b024aa5f187141a542ef834585",
+"pspatrac3_8h.html#af102179ca2f24aedd64be660c0f8fab4",
+"pspfpu_8h.html#a0977e8e446156da40c17fdc7b8eed65e",
+"pspgu_8h.html#a949b4f9d51c7d7de386f48cb0447bb81",
+"pspinit_8h.html#ae231e9326c485a4acbf0fa197fc758efa91628f23d798f52e38565839235788d2",
+"pspkerror_8h.html#ada38ce7dd0ddff755524e736afd6fcacad001531a53328b9ad999c4e6f3cfb3e1",
+"pspnet__adhocctl_8h.html#a66a00a1005dcfe2498bfea73102dbe55",
+"pspsysmem__kernel_8h.html#a5ac083a645d964373f022d03df4849c8",
+"pspusbcam_8h.html#a42945e4235d68f67d2b6450a025a0d2dab93dd286bf9b26de463658eacfe7ee82",
+"psputility__netparam_8h.html#ad242c1a86c0494fa278365a9b01e7346",
+"sceGuPatchPrim_8c.html",
+"string_8h.html#a21e4788b4a0ae39b11aa32e9a1079f98",
+"structPspUsbCamSetupStillParam.html#ad0229f3031c2197b419f835a99fab4d2",
+"structSceKernelThreadKInfo.html#aef027c763f7f7512988715c4e80e7986",
+"structScePspIMatrix4.html#ac935f32e6454435d4a19b2f27ccb3bf5",
+"struct__SceKernelUtilsSha1Context.html#a1170d3c578fa91ddd2dc40afce3f1131",
+"structtag__IntrHandlerOptionParam.html#a6a76ccd51e31c0fb3d876da0de059519"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
